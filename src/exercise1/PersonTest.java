e0d9ceb178d7434175c22f4f98e335160c573cad
daria.hauer <daria.hauer@aptitudesoftware.com>
2019-11-08 10:43:16 +0100

    scanner close for exercise1

--- src/exercise1/PersonTest.java
+++ src/exercise1/PersonTest.java
@@ -16,6 +16,7 @@ public class PersonTest {
             int pesel = input.nextInt();
             Person person = new Person(firstName, lastName, age, pesel);
             System.out.println(person);
+            input.close();
         } catch (NameUndefinedException | IncorrectAgeException ex) {
             System.out.println(ex.getMessage());
         }
